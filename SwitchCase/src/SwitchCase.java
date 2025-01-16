209ba3c4908dcf14ea8c462b5a60691251418b1f
Hanaellenoff <hanaellenoff@gmail.com>
2025-01-16 12:59:14 -0500

    SwitchCase

---
+++ SwitchCase/src/SwitchCase.java
@@ -0,0 +1,23 @@
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		String dog = "pomeranian";
+	 
+		switch(dog) {
+			case "pomeranian":
+			System.out.println("small dog"); 
+			break;
+			
+			case "great dane":
+				System.out.println("big dog"); 
+				break;
+				
+			default:
+				System.out.println("try a diff dog type");
+		}
+	}
+
+}
