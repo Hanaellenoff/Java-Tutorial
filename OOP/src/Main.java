a11e0ef5b8ea9dffb61f217591a901f968395276
Hanaellenoff <hanaellenoff@gmail.com>
2025-01-22 17:35:28 -0500

    classes

---
+++ OOP/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Pen p = new Pen();
+
+         System.out.println(p.color);	
+         System.out.println(p.point);	
+         System.out.println(p.type);	
+         
+         p.click();
+         System.out.println(p.clicked);	
+	}
+
+}
+
+
+
---
+++ OurProject/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class2 c = new Class2();
+		Class1 d = new Class1();
+	System.out.println(c.y);
+		
+	
+
+	}
+	
+	
+
+}
