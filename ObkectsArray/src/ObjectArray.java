a7e8cd9c9f550b3ea2f0e7583f1e5b657b518254
Hanaellenoff <hanaellenoff@gmail.com>
2025-01-22 18:23:10 -0500

    classes

---
+++ ObkectsArray/src/ObjectArray.java
@@ -0,0 +1,32 @@
+
+class Monkey {
+	String type = "Spider monkey";
+	
+	public static void sayOooAhhhAhh() {
+		
+		System.out.println("Ohh Ahh Ahh");
+	}
+}
+
+
+public class ObjectArray {
+
+	public static void main(String[] args) {
+		
+		
+		Monkey m1 = new Monkey();
+		Monkey m2 = new Monkey();
+		Monkey m3 = new Monkey();
+		
+		Monkey[] monkeys = {m1, m2, m3};
+		
+		int[] nums = {1, 4, 5};
+		
+		for(Monkey m : monkeys) {
+			m.sayOooAhhhAhh();
+		}
+		
+	}
+	
+
+}
