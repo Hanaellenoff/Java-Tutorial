9a46975f5c895d46d49699c431e7ca7b0cfd328b
Hanaellenoff <hanaellenoff@gmail.com>
2025-01-22 18:29:41 -0500

    shirt class

--- BobTheBuilder/src/Main.java
+++ BobTheBuilder/src/Main.java
@@ -3,14 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Class2 c = new Class2();
-		Class1 d = new Class1();
-	System.out.println(c.y );
+		Shirt s = new Shirt();
+		s.putOn();
 		
-	
-
 	}
-	
-	
 
 }
