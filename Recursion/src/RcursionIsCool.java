2d2e0dd4135c54f65ddbc180b8200a6bbdff6fbc
Hanaellenoff <hanaellenoff@gmail.com>
2025-01-21 23:03:39 -0500

    recursion

--- Recursion/src/RcursionIsCool.java
+++ Recursion/src/RcursionIsCool.java
@@ -3,7 +3,21 @@ public class RcursionIsCool {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		countBackwards(5);
 
+	}
+	
+	public static void countBackwards(int n) {
+		if (n == 0) {
+			System.out.println("done");
+		} else {
+			System.out.println(n);
+			n--;
+			countBackwards(n);
+		}
+		
+		
 	}
 
 }
