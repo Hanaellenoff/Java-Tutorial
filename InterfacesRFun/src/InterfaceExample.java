0e2453bf756d4553f5c4540904b8ce07634daf41
Hanaellenoff <hanaellenoff@gmail.com>
2025-01-23 12:03:11 -0500

    interfaces

---
+++ InterfacesRFun/src/InterfaceExample.java
@@ -0,0 +1,25 @@
+interface WaterBottleInterface {
+	String color = "blue";
+	
+	
+	void fillUp();
+}
+
+
+
+public class InterfaceExample implements WaterBottleInterface {
+	public static void main(String[] args) {
+		System.out.println(color);
+		
+		InterfaceExample ex = new InterfaceExample()
+;
+		ex.fillUp();
+	}
+
+	@Override
+	public void fillUp() {
+		// TODO Auto-generated method stub
+		System.out.println("Its filled up");
+	}
+
+}
--- Naming/src/NamingConventions.java
+++ Naming/src/NamingConventions.java
@@ -3,7 +3,10 @@ public class NamingConventions {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int a = 5;
+		 
 
 	}
-
+	 
 }
