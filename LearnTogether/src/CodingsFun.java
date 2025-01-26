f05d14c39f6059d8542b6a68c4bbf33a8bed240f
Hanaellenoff <hanaellenoff@gmail.com>
2025-01-26 18:57:53 -0500

    stuff

---
+++ LearnTogether/src/CodingsFun.java
@@ -0,0 +1,14 @@
+
+public class CodingsFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student s = new Student(1, "jill");
+		Student s2 = new Student(2, "jack");
+		
+		System.out.println(s);
+		System.out.println(s2);
+	}
+
+}
