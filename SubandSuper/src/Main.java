5502aed4ee8227b6dcab3eaf741a0a22895439b5
Hanaellenoff <hanaellenoff@gmail.com>
2025-01-26 18:22:09 -0500

    edit

---
+++ SubandSuper/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Walking s = new Walking(true, "Nike", 1.0);
+			System.out.println(s.gorTex);
+
+	}
+
+}
--- SubandSuper/src/Walking.java
+++ SubandSuper/src/Walking.java
@@ -4,6 +4,7 @@ public class Walking extends Shoe{
 	public final boolean gorTex;
 	
 	Walking (boolean gorTex, String brand, Double size) {
-	this.gorTex = gorTex;
+		super(brand, size);
+	    this.gorTex = gorTex;
 	}
 }
